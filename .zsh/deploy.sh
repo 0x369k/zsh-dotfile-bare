bf9be826118761cb52b461eda38ef09aa83a6b4e
qgrep <oremek@raphael-christopher.de>
2023-08-12 19:35:31 +0200

    update

--- .zsh/deploy.sh
+++ .zsh/deploy.sh
@@ -1,69 +1,83 @@
 #!/bin/bash
-  printf "%s\n" \
-    '
-   @@@@@@@   @@@@@@  @@@@@@@ @@@@@@@@ @@@ @@@      @@@@@@@@  @@@@@@
-   @@!  @@@ @@!  @@@   @@!   @@!      @@! @@!      @@!      !@@    
-   @!@  !@! @!@  !@!   @!!   @!!!:!   !!@ @!!      @!!!:!    !@@!! 
-   !!:  !!! !!:  !!!   !!:   !!:      !!: !!:      !!:          !:!
-   :: :  :   : :. :     :     :       :   : ::.: : : :: ::: ::.: : 
-      '
+printf "%s\n" \
+'
+@@@@@@@   @@@@@@  @@@@@@@ @@@@@@@@ @@@ @@@      @@@@@@@@  @@@@@@
+@@!  @@@ @@!  @@@   @@!   @@!      @@! @@!      @@!      !@@
+@!@  !@! @!@  !@!   @!!   @!!!:!   !!@ @!!      @!!!:!    !@@!!
+!!:  !!! !!:  !!!   !!:   !!:      !!: !!:      !!:          !:!
+:: :  :   : :. :     :     :       :   : ::.: : : :: ::: ::.: :
+'
+
+
 #------------------------------------------------------------------------------#
 # SETTINGS
 #------------------------------------------------------------------------------#
-DOTDIR="$HOME/.dotfiles"
-BACKUPDIR="$HOME/.dotfiles.backup"
+
+DOTDIR=".dotfiles"
+BACKUPDIR=".dotfiles.backup"
+
 #------------------------------------------------------------------------------#
 # check Download or UPDATE
 #------------------------------------------------------------------------------#
+
 alias dot='git --git-dir=$HOME/$DOTDIR --work-tree=$HOME $@'
-dot() { git --git-dir=".dotfiles" --work-tree="$HOME" "$@"; }
+dot() { git --git-dir="$DOTDIR" --work-tree="$HOME" "$@"; }
 
-#if [ -d "$DOTDIR" ]; then
-#echo "> updating dotfiles..."
-#  dot pull 
-#  exit 0
-#fi
-#------------------------------------------------------------------------------#
-# Backup already existing dotfiles
-#------------------------------------------------------------------------------#
-DOTGITFILES=($(dot ls-tree --full-tree -r --name-only HEAD | awk '{print $NF}'))
 
-for f in "${DOTGITFILES[@]}"; do
-  # File at root ==> back up file
-  if [[ $(basename "$f") = "$f" ]]; then
-    [[ -f "$HOME/$f" ]] && mkdir -p "$BACKUPDIR" && mv "$HOME/$f" "$BACKUPDIR" && echo "> Backing up: $f ==> $BACKUPDIR/$f"
-  else
-    d=${f%%/*}
-    if [[ -d "$HOME/$d" ]]; then
-      [[ -d "$BACKUPDIR/$d" ]] && rm -rf "$BACKUPDIR/$d"
-      mkdir -p "$BACKUPDIR" && mv "$HOME/$d" "$BACKUPDIR" && echo "> Backing up: $d/ ==> $BACKUPDIR/$d/"
-    fi
-  fi
-done
-[ -d "$BACKUPDIR" ] && echo "> Success! The following dotfiles have been installed to $BACKUPDIR"
+if [ -d "$DOTDIR" ]; then
+echo "> updating dotfiles..."
+dot pull
+exit 0
+fi
+
+echo "> downloading dotfiles..."
+
 #------------------------------------------------------------------------------#
 # DOWNLOAD dotfiles
 #------------------------------------------------------------------------------#
-echo "> downloading dotfiles..."
-dot clone --bare https://github.com/qgrep/zsh-dotfile-bare $DOTDIR
+
+
+dot clone --bare https://github.com/qgrep/zsh-dotfile-bare $HOME/$DOTDIR
 #dot clone --bare --recurse-submodules https://github.com/qgrep/zsh-dotfile-bare.git "$HOME/.dotfiles"
+
+#------------------------------------------------------------------------------#
+# Backup already existing dotfiles
+#------------------------------------------------------------------------------#
+
+DOTGITFILES=($(dot ls-tree --full-tree -r --name-only HEAD | awk '{print $NF}'))
+
+for f in "${DOTGITFILES[@]}"; do
+# File at root ==> back up file
+if [[ $(basename "$f") = "$f" ]]; then
+[[ -f "$HOME/$f" ]] && mkdir -p "$HOME/$BACKUPDIR" && mv "$HOME/$f" "$HOME/$BACKUPDIR" && echo "> Backing up: $f ==>$BACKUPDIR/$f"
+else
+d=${f%%/*}
+if [[ -d "$HOME/$d" ]]; then
+[[ -d "$HOME/$BACKUPDIR/$d" ]] && rm -rf "$HOME/$BACKUPDIR/$d"
+mkdir -p "$HOME/$BACKUPDIR" && mv "$HOME/$d" "$HOME/$BACKUPDIR" && echo "> Backing up: $d/ ==> $BACKUPDIR/$d/"
+fi
+fi
+done
+
 #------------------------------------------------------------------------------#
 # INSTALL dotfiles
 #------------------------------------------------------------------------------#
-[[ -f $ZDOTDIR/.zshrc ]] && . $ZDOTDIR/.zshrc
+
 dot checkout
 dot submodule --quiet init
 dot submodule --quiet update
 dot config status.showUntrackedFiles no
+[ -d "$BACKUPDIR" ] && echo "> Success! The following dotfiles have been installed to $BACKUPDIR"
+
 
 
 #------------------------------------------------------------------------------#
 # DEV
 #------------------------------------------------------------------------------#
-  # if [ ! -d "$HOME/.oh-my-zsh" ]; then
-  #    sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
-  #     ZSHPACKAGES=("https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/themes/powerlevel10k \
-  #                   https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting \
-  #                   https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions \
-  #                   https://github.com/zsh-users/zsh-completions ${ZSH_CUSTOM:-${ZSH:-~/.oh-my-zsh}/custom}/plugins/zsh-completions")
-  #fi
+# if [ ! -d "$HOME/.oh-my-zsh" ]; then
+#    sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+#     ZSHPACKAGES=("https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/themes/powerlevel10k \
+#                   https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting \
+#                   https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions \
+#                   https://github.com/zsh-users/zsh-completions ${ZSH_CUSTOM:-${ZSH:-~/.oh-my-zsh}/custom}/plugins/zsh-completions")
+#fi
