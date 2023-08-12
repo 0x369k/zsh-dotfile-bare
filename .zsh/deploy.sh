2411b64dc17a3fbc0af23f1c5fbda4f36044fbda
qgrep <oremek@raphael-christopher.de>
2023-08-12 18:48:07 +0200

    update

--- .zsh/deploy.sh
+++ .zsh/deploy.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+  #!/bin/bash
+
   printf "%s\n" \
     '
    @@@@@@@   @@@@@@  @@@@@@@ @@@@@@@@ @@@ @@@      @@@@@@@@  @@@@@@
@@ -7,25 +9,43 @@
    !!:  !!! !!:  !!!   !!:   !!:      !!: !!:      !!:          !:!
    :: :  :   : :. :     :     :       :   : ::.: : : :: ::: ::.: : 
       '
+
+
 #------------------------------------------------------------------------------#
 # SETTINGS
 #------------------------------------------------------------------------------#
+
 DOTDIR=".dotfiles"
 BACKUPDIR=".dotfiles.backup"
+
 #------------------------------------------------------------------------------#
 # check Download or UPDATE
 #------------------------------------------------------------------------------#
+
 alias dot='git --git-dir=$HOME/$DOTDIR --work-tree=$HOME $@'
 dot() { git --git-dir="$DOTDIR" --work-tree="$HOME" "$@"; }
+
 
 if [ -d "$DOTDIR" ]; then
 echo "> updating dotfiles..."
   dot pull 
   exit 0
 fi
+
+echo "> downloading dotfiles..."
+
+#------------------------------------------------------------------------------#
+# DOWNLOAD dotfiles
+#------------------------------------------------------------------------------#
+
+
+  dot clone --bare https://github.com/qgrep/zsh-dotfile-bare $HOME/$DOTDIR 
+  #dot clone --bare --recurse-submodules https://github.com/qgrep/zsh-dotfile-bare.git "$HOME/.dotfiles"
+
 #------------------------------------------------------------------------------#
 # Backup already existing dotfiles
 #------------------------------------------------------------------------------#
+
 DOTGITFILES=($(dot ls-tree -r HEAD | awk '{print $NF}'))
 
 for f in "${DOTGITFILES[@]}"; do
@@ -40,21 +60,17 @@ for f in "${DOTGITFILES[@]}"; do
     fi
   fi
 done
-[ -d "$BACKUPDIR" ] && echo "> Success! The following dotfiles have been installed to $BACKUPDIR"
-#------------------------------------------------------------------------------#
-# DOWNLOAD dotfiles
-#------------------------------------------------------------------------------#
-echo "> downloading dotfiles..."
-dot clone --bare https://github.com/qgrep/zsh-dotfile-bare $HOME/$DOTDIR
-#dot clone --bare --recurse-submodules https://github.com/qgrep/zsh-dotfile-bare.git "$HOME/.dotfiles"
+
 #------------------------------------------------------------------------------#
 # INSTALL dotfiles
 #------------------------------------------------------------------------------#
-[[ -f $ZDOTDIR/.zshrc ]] && . $ZDOTDIR/.zshrc
+
 dot checkout
 dot submodule --quiet init
 dot submodule --quiet update
 dot config status.showUntrackedFiles no
+[ -d "$BACKUPDIR" ] && echo "> Success! The following dotfiles have been installed to $BACKUPDIR"
+
 
 
 #------------------------------------------------------------------------------#
